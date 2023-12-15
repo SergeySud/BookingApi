f38a3bcc8818f431a61a614ab83d9204fa7688cb
SergeySud <56123027+SergeySud@users.noreply.github.com>
2023-12-15 13:14:25 +0300

    Update views.py

--- booking/views.py
+++ booking/views.py
@@ -88,7 +88,7 @@ class ReservationView(generics.ListCreateAPIView):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    def delete(self, request, *args, **kwargs):
+        def destroy(self, request, *args, **kwargs):
         reservation_id = request.data.get('id', None)
         if reservation_id is not None:
             reservations = Reservation.objects.filter(id=reservation_id)
